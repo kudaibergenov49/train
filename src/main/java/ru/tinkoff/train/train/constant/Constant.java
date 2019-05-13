357d803fd18e60969c6aa3b9694e8b3e27c1123c
Kuanysh Kudaibergen <k.kudaibergen@mit.ooo>
2019-05-13 15:34:57 +0300

    fix Constant class

--- src/main/java/ru/tinkoff/train/train/constant/Constant.java
+++ src/main/java/ru/tinkoff/train/train/constant/Constant.java
@@ -1,10 +1,10 @@
 package ru.tinkoff.train.train.constant;
 
-public interface Constant {
-    Character switchOf = '0';
-    Character switchOn = '1';
-    String firstTrainValue = "100";
-    String secondTrainValue = "001";
-    Integer firstTrainCount = 3;
-    Integer secondTrainCount = 3;
+public class Constant {
+    public static final Character switchOf = '0';
+    public static final Character switchOn = '1';
+    public static final String firstTrainValue = "100";
+    public static final String secondTrainValue = "001";
+    public static final Integer firstTrainCount = 3;
+    public static final Integer secondTrainCount = 3;
 }
